56d77c7ded2a673f67ab3b202f9cc879736cb3e1
zcatbear <jamieford34@gmail.com>
2018-01-04 21:58:07 -0600

    Prints out extra spaces between the two messages

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     timeNow = str(datetime.utcnow())
-    return f'Hello from a container\n' \
-           f'The current time is {timeNow}\n'
+    return f'Hello from a container\n\n\n\n The current time is {timeNow}\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
