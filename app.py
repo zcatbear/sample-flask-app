3af7b5e7f5e55a6f0ad10b209451878a703e020a
zcatbear <jamieford34@gmail.com>
2018-01-04 21:54:30 -0600

    Adds datetime to the message printout and utilizes f-strings

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from a container"
+    timeNow = str(datetime.utcnow())
+    return f'Hello from a container\n' \
+           f'The current time is {timeNow}\n'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
