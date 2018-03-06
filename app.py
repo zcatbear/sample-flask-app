06b4750d9fe0cf26aa0c88bef777d1b7baaecc08
zcatbear <jamieford34@gmail.com>
2018-03-05 21:02:46 -0600

    Puts in parens for python 3.5

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        print request.headers
+        print(request.headers)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
