a30cf9b669a920dddb9dc201a949eb2e4e0cf8e8
Jamie Ford <jamie@Jamies-MacBook-Pro.local>
2018-09-11 21:50:51 -0500

    Fixes app.py issue where it was causing 500 errors

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        print(request.headers)
+    #print(request.headers)
+    return str(request.headers)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
